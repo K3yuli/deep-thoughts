af63f48b195bf5999c392f0d63785a81bd4843b2
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-03-24 12:13:50 -0400

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,41 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
+    // look up thoughts by username
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        thoughts: async (parent, { username }) => {
+            // checking if username exists if not return
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+    },
+    // find a single thought
+    thought: async(parent, { _id }) => {
+        return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+        return User.find()
+        .select('__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+        return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            // mongoose User model creates a new user in the database with whatever is passed in as the args
+            const user = await User.create(args);
+
+            return user;
+        },
+        login: async () => {
+
         }
     }
 };
